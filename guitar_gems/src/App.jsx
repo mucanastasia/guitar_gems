65df6c317bfbec001437da0c1554aa31cba85be1
mucanastasia <44086697+mucanastasia@users.noreply.github.com>
2024-07-08 13:08:34 +0200

    Setting up deploy on GitHub pages

--- guitar_gems/src/App.jsx
+++ guitar_gems/src/App.jsx
@@ -1,6 +1,6 @@
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
-// import Product from './components/product/Product';
+import Product from './components/product/Product';
 import Catalogue from './components/catalogue/Catalogue';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SingUp';
@@ -12,12 +12,18 @@ export default function App() {
     <>
       <Header />
       <Catalogue />
-      {/* <Product guitarId={2} /> */}
       <Footer />
 
       <AuthPage>
         <SignIn />
       </AuthPage>
+
+      <AuthPage>
+        <SignUp />
+      </AuthPage>
+
+      <Product guitarId={2} />
+      <Footer />
     </>
   );
 }
