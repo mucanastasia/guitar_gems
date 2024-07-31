48a04e28aecb96b66f068c32d553407c7b0ae002
mucanastasia <44086697+mucanastasia@users.noreply.github.com>
2024-07-31 22:49:02 +0200

    Setup React Query

--- guitar_gems/src/main.jsx
+++ guitar_gems/src/main.jsx
@@ -2,9 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { SessionProvider } from '@features/auth/contexts/SessionContext.jsx';
 import { Router } from '@features/router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 const App = () => {
-	return <Router />;
+	return (
+		<QueryClientProvider client={queryClient}>
+			<Router />
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
+	);
 };
 
 ReactDOM.createRoot(document.getElementById('root')).render(
