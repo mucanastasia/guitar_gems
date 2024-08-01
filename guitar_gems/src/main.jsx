322792d07e6a37c2b6807dd7109f1e275f945fad
mucanastasia <44086697+mucanastasia@users.noreply.github.com>
2024-08-01 21:52:51 +0200

    Delete SessionContext

--- guitar_gems/src/main.jsx
+++ guitar_gems/src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { SessionProvider } from '@features/auth/contexts/SessionContext.jsx';
 import { Router } from '@features/router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
@@ -18,8 +17,6 @@ const App = () => {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<SessionProvider>
-			<App />
-		</SessionProvider>
+		<App />
 	</React.StrictMode>
 );
