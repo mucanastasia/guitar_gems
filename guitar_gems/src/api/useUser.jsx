9253604dcd8a58e99d10561df3315a7a130c559b
mucanastasia <44086697+mucanastasia@users.noreply.github.com>
2024-08-08 00:01:34 +0200

    delete logs

--- guitar_gems/src/api/useUser.jsx
+++ guitar_gems/src/api/useUser.jsx
@@ -2,11 +2,9 @@ import { useQuery } from '@tanstack/react-query';
 import { supabase } from '@api/supabaseClient';
 
 const getUser = async () => {
-	console.log('starting getUser');
 	const {
 		data: { user },
 	} = await supabase.auth.getUser();
-	console.log('finishing getUser', user);
 	return user;
 };
 
