0206da56503870ccdf1f333546738cded6457715
mucanastasia <44086697+mucanastasia@users.noreply.github.com>
2024-08-07 13:02:44 +0200

    Move global styles from AppLayout.css to main.css

--- guitar_gems/src/features/router/components/app-layout/AppLayout.jsx
+++ guitar_gems/src/features/router/components/app-layout/AppLayout.jsx
@@ -1,13 +1,12 @@
 import { Header } from '@features/header';
 import { Footer } from '@features/footer';
-import './AppLayout.css';
 
 export function AppLayout({ children, shouldShowFooter, shouldShowHeader }) {
 	return (
 		<div className="app-layout">
 			{shouldShowHeader && <Header />}
 
-			<main className="main-content">{children}</main>
+			<main>{children}</main>
 
 			{shouldShowFooter && <Footer />}
 		</div>
--- guitar_gems/src/main.jsx
+++ guitar_gems/src/main.jsx
@@ -4,6 +4,7 @@ import { Router } from '@features/router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { AuthProvider } from '@features/auth/context/AuthContext';
+import './main.css';
 
 const queryClient = new QueryClient({
 	defaultOptions: {
